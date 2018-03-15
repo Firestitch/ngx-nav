9e37b30f6e0d193539953e295a5380c68aa75254
Raymond <ray@firestitch.com>
2018-03-15 07:57:09 -0400

    cleaned up demo

--- playground/app/components/tabs/inputs-data/inputs-data.component.ts
+++ playground/app/components/tabs/inputs-data/inputs-data.component.ts
@@ -23,9 +23,56 @@ export class InputsDataComponent implements OnInit, OnDestroy {
 
   public ngOnInit() {
     this.stack.setTitle('Data inputs');
+    
     this.stack.setAction(() => {
       console.log('Action "Data inputs" was clicked');
     });
+
+    // this.stack.setAction({
+    //   menu: false,
+    //   label: 'Save',
+    //   class: 'md-primary',
+    //   click: function () {
+    //     console.log('Clicked Saved');
+    //   }
+    // });    
+
+    // this.stack.setActions([
+    //   //Save text button placed outside of menu
+    //   {
+    //     menu: false,
+    //     label: 'Save',
+    //     class: 'md-primary',
+    //     click: function () {
+    //       console.log('Clicked Saved');
+    //     }
+    //   },
+    //   //Settings icon button placed outside of menu
+    //   {
+    //     icon: 'settings',        
+    //     label: 'Settings',
+    //     click: function () {
+    //       console.log('Clicked Settings');
+    //     }
+    //   },
+    //   //Edit text menu item placed inside menu
+    //   {
+    //     label: 'Edit',
+    //     menu: true,
+    //     click: function () {
+    //       console.log('Clicked Edit');
+    //     }
+    //   },
+    //   //Delete icon/text menu item placed inside menu
+    //   {
+    //     label: 'Delete',
+    //     icon: 'delete',
+    //     menu: true,
+    //     click: function () {
+    //       console.log('Clicked Delete');
+    //     }
+    //   }
+    // ]);
 
     console.log('init inputs page');
   }
