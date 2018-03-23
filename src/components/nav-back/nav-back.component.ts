8d84d3097276dcc1c3ad36e5e74b51c57f0ce0fd
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-03-23 15:55:30 +0300

    GO-T359 nav components for back button

---
+++ src/components/nav-back/nav-back.component.ts
@@ -0,0 +1,67 @@
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  OnDestroy,
+  OnInit,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { pairwise } from 'rxjs/operators';
+
+import { FsNavRouteHandleService } from '../../services';
+
+import { UrlInfo } from '../../interfaces';
+
+
+@Component({
+  selector: '[fsNavBack]',
+  templateUrl: 'nav-back.component.html'
+})
+export class FsNavBackComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  public routeInfo: UrlInfo = {};
+
+  private _routerSubscription;
+  private _backClickListener;
+
+  @ContentChild('backButton') public backButton: ElementRef;
+
+
+  constructor(
+    private _stack: FsNavRouteHandleService,
+    private _router: Router,
+    private _renderer: Renderer2,
+  ) {}
+
+  public ngOnInit() {
+    this.createRouterSubscription();
+  }
+
+  public ngAfterViewInit() {
+    this.createBackClickListener();
+  }
+
+  public ngOnDestroy() {
+    this._routerSubscription.unsubscribe();
+    this._backClickListener();
+  }
+
+  public createRouterSubscription() {
+    this._routerSubscription = this._router.events
+      .pipe(pairwise())
+      .subscribe(([prevRouteEvent, currRouteEvent]) => {
+        if (currRouteEvent instanceof NavigationEnd) {
+          this.routeInfo = this._stack.getActiveRouteInfo();
+        }
+    });
+  }
+
+  public createBackClickListener() {
+    this._backClickListener = this._renderer.listen(this.backButton.nativeElement, 'click', (evt) => {
+      this._router.navigateByUrl(this._stack.goBack());
+    });
+  }
+}
